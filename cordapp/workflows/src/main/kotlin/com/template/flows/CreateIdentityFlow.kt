b66b71b70bd9a92c6a9425384aea43614b9fb7f2
Thiago Cardozo <tcardozo@bluchain.com>
2019-06-08 20:43:05 -0300

    fixes

--- cordapp/workflows/src/main/kotlin/com/template/flows/CreateIdentityFlow.kt
+++ cordapp/workflows/src/main/kotlin/com/template/flows/CreateIdentityFlow.kt
@@ -1,28 +1,27 @@
 package com.template.flow
 
 import co.paralleluniverse.fibers.Suspendable
-import com.template.contract.AccountContract
+import com.template.contract.IdentityContract
 import net.corda.core.contracts.Command
 import net.corda.core.flows.*
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
-import com.template.model.AccountModel
-import com.template.state.AccountState
+import com.template.model.IdentityModel
+import com.template.state.IdentityState
 import java.time.Instant
 
-import okhttp3.OkHttpClient
-import okhttp3.Request
+//import okhttp3.OkHttpClient
+//import okhttp3.Request
 
 object CreateAccount {
 
     @InitiatingFlow
     @StartableByRPC
-    class CreateAccountFlow(
+    class CreateIdentityFlow(
             val uid : String,
             val name: String,
-            val did : String,
-            val balance : Double
+            val did : String
     ) : BaseFlow() {
 
         companion object {
@@ -54,33 +53,33 @@ object CreateAccount {
             // define progresso
             progressTracker.currentStep = INITIALISING
 
-//             testar se a DID informada está correta
-            val validateUrl = "http://localhost:10050/api/verifyDid/did:sov:23h23kskdl" // ????
-            val httpRequest = Request.Builder().url(validateUrl).build()
-            val httpResponse = OkHttpClient().newCall(httpRequest).execute()
-            if (!httpResponse.isSuccessful) { throw Exception("DID Inválida") }
+//          testar se a DID informada está correta
+//            val validateUrl = "http://localhost:10050/api/verifyDid/did:sov:23h23kskdl" // ????
+//            val httpRequest = Request.Builder().url(validateUrl).build()
+//            val httpResponse = OkHttpClient().newCall(httpRequest).execute()
+//            if (!httpResponse.isSuccessful) { throw Exception("DID Inválida") }
+//
+//            val accountList = getAccountStateByDocument(this.uid)
+//            if (accountList.isNotEmpty()) {
+//                throw Exception("Conta existente")
+//            }
 
-            val accountList = getAccountStateByDocument(this.uid)
-            if (accountList.isNotEmpty()) {
-                throw Exception("Conta existente")
-            }
 
             // define uma conta
-            val account = AccountModel(
+            val account = IdentityModel(
                     entity = ourIdentity,
                     entityName = ourIdentity.name.organisation,
                     createTime = Instant.now(),
                     name = name,
                     uid = uid,
-                    did = did,
-                    balance = balance)
+                    did = did)
 
             // cria o state
-            val accountState = AccountState(account)
+            val accountState = IdentityState(account)
 
             // criando o command para validação do contract
             val txCommand = Command(
-                AccountContract.Commands.CreateAccount(),
+                IdentityContract.Commands.CreateId(),
                 accountState.participants.map { it.owningKey }
             )
 
@@ -89,7 +88,7 @@ object CreateAccount {
             // criando a transação e validando
             val txBuilder = TransactionBuilder(notary)
                 .addCommand(txCommand)
-                .addOutputState(accountState, AccountContract::class.java.canonicalName)
+                .addOutputState(accountState, IdentityContract::class.java.canonicalName)
 
             txBuilder.verify(serviceHub)
 
