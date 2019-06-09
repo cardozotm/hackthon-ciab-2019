b5a2bd0c3f00987ad2a4b26ee97f3ca464f85d9e
Francisco Llamosas Baptista <fbaptista@bluchain.tech>
2019-06-09 03:56:39 -0300

    add user counter

--- userApp/src/app/pages/summary/summary.page.ts
+++ userApp/src/app/pages/summary/summary.page.ts
@@ -10,6 +10,7 @@ import * as moment from 'moment';
 export class SummaryPage implements OnInit {
   averageAge: number;
   averageIncome: number;
+  totalUsers = 0;
 
   constructor(
     private cordaSv: CordaService
@@ -22,21 +23,20 @@ export class SummaryPage implements OnInit {
   getAllStates() {
     this.cordaSv.getAll()
     .then((states: Array<any>) => {
-      let counter = 0;
       let sumAge = 0;
       let sumIncome = 0;
       states.forEach(el => {
         const perData = JSON.parse(el.state.data.identity.personalData);
         const finData = JSON.parse(el.state.data.identity.financialData);
-        counter += 1;
+        this.totalUsers += 1;
         const age = moment(perData.birthDate.split('/').reverse()).fromNow(true).match(/\d+/g).map(Number)[0];
         const income = finData.anualIncome;
-        sumIncome += income
+        sumIncome += income;
         sumAge += age;
         console.log(sumAge);
       });
-      this.averageAge = sumAge / counter;
-      this.averageIncome = sumIncome / counter;
+      this.averageAge = sumAge / this.totalUsers;
+      this.averageIncome = sumIncome / this.totalUsers;
     });
   }
 
